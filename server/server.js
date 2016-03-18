77052fc5a8631a78a7265a8d3f69bf73279f6d56
benstuijts <stuijts@spijkenisse.nl>
2016-03-18 20:12:09 +0100

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* Dependencies */
+  const http        = require('http');
+  const express     = require('express');
+  const app         = express();
+  const server      = http.createServer(app);
+  
+  const port        = process.env.PORT || 8080;
+  const host        = process.env.IP;
+
+  const cookieParser= require('cookie-parser');
+  const bodyParser  = require('body-parser');
+  const session     = require('express-session');
+  const mongoose    = require('mongoose');
+  
+/* Configuration */
+  //const configDb = require('./config/database.js');
+
+/* Database */
+  //mongoose.connect(configDb.localhost.url, function(err){
+  //    if(err) {console.log('! NOT Connected to database');}
+  //    console.log('# Connected to database: ' + configDb.localhost.name);
+  //});
+  // Make our db accessible to our router
+  //app.use(function(req,res,next){
+  //    req.db = 'database instance';
+  //    next();
+  //});
+
+/* View Engine */
+  app.set('views', './views');
+  app.set('view engine', 'ejs');
+
+/* Middleware */
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(session({ secret: 'any string', saveUninitialized: true, resave: true, cookie: { maxAge: 120000 }}));
+  app.use(express.static('./public_html'));
+
+server.listen(port,function() {
+  console.log(Date.now() + ' | server is running...');
+});
+
+/* Routes */
+
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+//app.use('/test', require('./routes/test_routes'));
+//app.use('/auth', require('./routes/auth_routes'));
+//require('./routes/game_routes')(app);
+//require('./routes/admin_routes')(app);
